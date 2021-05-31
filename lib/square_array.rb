de76c6344127bd08b6593499b38fd7a59a57bb3a
dynamic-daemon-637b <zl3087@columbia.edu>
2021-05-31 00:52:36 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,8 @@
 def square_array(array)
-  new = []
-  array.length.times do |index|
-   new.push(array[index]**2)
- end
- new
+new_array = []
+ 
+array.length.times do |index|
+  new_array.push(array[index] + 1)
+end
+new_array
+ # => [2, 3, 4, 5, 6]
